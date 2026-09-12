2711413382f961221374de23021761890f39efb7
jagadish <jagadishthota912@gmail.com>
2026-09-12 12:47:42 +0530

    Base_url

--- src/services.js
+++ src/services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://furniturestore-1.onrender.com";
 const API = `${BASE_URL}/products`;
 
 export const getProducts = () => axios.get(API);
